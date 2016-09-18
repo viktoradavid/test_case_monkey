2040c42d5e760d1c3b430a5abe21fa93c9c4b7c3
David Viktora <viktora.david@gmail.com>
2016-09-18 22:39:47 +0200

    Add tcm models to admin

--- test_case_monkey/tcm/admin.py
+++ test_case_monkey/tcm/admin.py
@@ -1,8 +1,31 @@
 from django.contrib import admin
 
-from .models import Organisation
+from . import models
 
-class OrganisationAdmin(admin.ModelAdmin):
-    pass
 
-admin.site.register(Organisation, OrganisationAdmin)
+class TestScenarioTemplateAttributeInline(admin.TabularInline):
+    model = models.TestScenarioTemplateAttribute
+
+
+class TestScenarioTemplateAdmin(admin.ModelAdmin):
+    inlines = (TestScenarioTemplateAttributeInline, )
+
+
+class TestScenarioAttributeInline(admin.TabularInline):
+    model = models.TestScenarioAttribute
+
+
+class TestScenarioAdmin(admin.ModelAdmin):
+    inlines = (TestScenarioAttributeInline, )
+
+
+admin.site.register(models.Organisation)
+admin.site.register(models.Project)
+admin.site.register(models.TestRun)
+admin.site.register(models.TestScenarioTemplate, TestScenarioTemplateAdmin)
+admin.site.register(models.TestScenario, TestScenarioAdmin)
+# admin.site.register(models.TestScenarioTemplateAttribute)
+# admin.site.register(models.TestScenarioAttribute)
+admin.site.register(models.TestCaseTemplate)
+admin.site.register(models.TestCase)
+admin.site.register(models.Bug)
