731f11355646e7136c4c45568496768e545edccb
David Viktora <viktora.david@gmail.com>
2016-09-18 10:52:50 +0200

    Add first draft of tcm's urls.py

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
     url(r'^accounts/', include('allauth.urls')),
 
     # Your stuff: custom urls includes go here
+    url(r'^tcm/', include('test_case_monkey.tcm.urls')),
 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ test_case_monkey/tcm/urls.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import url
+
+from . import views
+
+# TODO remove after creating real view
+from django.views import defaults as default_views
+
+urlpatterns = [
+    # TODO tcm index, list of orgs or redirect to one specific org
+    url(r'^$', default_views.page_not_found, kwargs={'exception': Exception('View yet to be created. ')}),
+
+    # TODO list of org's options
+    url(r'^(?P<org_short>[\w]+)/$', default_views.page_not_found, kwargs={'exception': Exception('View yet to be created. ')}),
+
+    # TODO list of org's projects
+    url(r'^(?P<org_short>[\w]+)/projects$', default_views.page_not_found, kwargs={'exception': Exception('View yet to be created. ')}),
+
+    # TODO creation of org's projects
+    url(r'^(?P<org_short>[\w]+)/projects/create$', default_views.page_not_found, kwargs={'exception': Exception('View yet to be created. ')}),
+
+    # TODO edit project details
+    url(r'^(?P<org_short>[\w]+)/projects/(?P<project_id>\d+)/edit$', default_views.page_not_found, kwargs={'exception': Exception('View yet to be created. ')}),
+
+    # TODO list of project's scenarios templates
+    url(r'^(?P<org_short>[\w]+)/projects/(?P<project_id>\d+)/scenarios$', default_views.page_not_found, kwargs={'exception': Exception('View yet to be created. ')}),
+
+    # TODO create scenario templates
+    url(r'^(?P<org_short>[\w]+)/projects/(?P<project_id>\d+)/scenarios/create$', default_views.page_not_found, kwargs={'exception': Exception('View yet to be created. ')}),
+
+    # TODO list of scenarios's testcases templates
+    url(r'^(?P<org_short>[\w]+)/projects/(?P<project_id>\d+)/scenarios/(?P<scenario_id>\d+)$', default_views.page_not_found, kwargs={'exception': Exception('View yet to be created. ')}),
+
+    # TODO edit scenario template
+    url(r'^(?P<org_short>[\w]+)/projects/(?P<project_id>\d+)/scenarios/(?P<scenario_id>\d+)/edit$', default_views.page_not_found, kwargs={'exception': Exception('View yet to be created. ')}),
+
+    # TODO create scenario's testcase template
+    url(r'^(?P<org_short>[\w]+)/projects/(?P<project_id>\d+)/scenarios/(?P<scenario_id>\d+)$/create', default_views.page_not_found, kwargs={'exception': Exception('View yet to be created. ')}),
+
+    # TODO list of project's testruns
+    url(r'^(?P<org_short>[\w]+)/projects/(?P<project_id>\d+)/testruns$', default_views.page_not_found, kwargs={'exception': Exception('View yet to be created. ')}),
+
+    # TODO test run detail page
+    url(r'^(?P<org_short>[\w]+)/projects/(?P<project_id>\d+)/testruns/(?P<testrun_id>\d+)$', default_views.page_not_found, kwargs={'exception': Exception('View yet to be created. ')}),
+
+    # TODO test run edit page
+    url(r'^(?P<org_short>[\w]+)/projects/(?P<project_id>\d+)/testruns/(?P<testrun_id>\d+)/edit$', default_views.page_not_found, kwargs={'exception': Exception('View yet to be created. ')}),
+
+    # TODO list of testrun's scenarios
+    url(r'^(?P<org_short>[\w]+)/projects/(?P<project_id>\d+)/testruns/(?P<testrun_id>\d+)/scenarios$', default_views.page_not_found, kwargs={'exception': Exception('View yet to be created. ')}),
+
+    # TODO edit of testrun's scenarios
+    url(r'^(?P<org_short>[\w]+)/projects/(?P<project_id>\d+)/testruns/(?P<testrun_id>\d+)/scenarios/edit$', default_views.page_not_found, kwargs={'exception': Exception('View yet to be created. ')}),
+
+    # TODO list of scenarios's testcases
+    url(r'^(?P<org_short>[\w]+)/projects/(?P<project_id>\d+)/testruns/(?P<testrun_id>\d+)/scenarios/(?P<scenario_id>\d+)$', default_views.page_not_found, kwargs={'exception': Exception('View yet to be created. ')}),
+
+    # TODO edit of scenarios's testcases
+    url(r'^(?P<org_short>[\w]+)/projects/(?P<project_id>\d+)/testruns/(?P<testrun_id>\d+)/scenarios/(?P<scenario_id>\d+)/edit$', default_views.page_not_found, kwargs={'exception': Exception('View yet to be created. ')}),
+
+]
